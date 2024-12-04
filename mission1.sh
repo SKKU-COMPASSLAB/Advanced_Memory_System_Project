248fbae6af20bf6769b08697c32d979acf2f0363
junhyeok <vzx00990@gmail.com>
2024-12-04 11:20:43 +0900

    Correcting typos

--- mission1.sh
+++ mission1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 run.py "m1_RDC16_RP16"
+python3 run.py "m1_RCD16_RP16"
 sleep 3
 
 echo "Re-compile, Wait"
@@ -9,7 +9,7 @@ sleep 3
 python3 modify.py "./src/DDR4.h" "203" "        {2400, (400.0/3)*9, (3/0.4)/9, 4, 4, 6, 2, 16, 20, 20, 12, 39, 55, 9, 3, 9, 18, 0, 0, 0, 0, 0, 6, 8, 0, 7, 0, 0},"
 make -j4
 sleep 10
-python3 run.py "m1_RDC20_RP20"
+python3 run.py "m1_RCD20_RP20"
 sleep 3
 
 echo "Re-compile, Wait"
@@ -18,7 +18,7 @@ sleep 3
 python3 modify.py "./src/DDR4.h" "203" "        {2400, (400.0/3)*9, (3/0.4)/9, 4, 4, 6, 2, 16, 24, 24, 12, 39, 55, 9, 3, 9, 18, 0, 0, 0, 0, 0, 6, 8, 0, 7, 0, 0},"
 make -j4
 sleep 10
-python3 run.py "m1_RDC24_RP24"
+python3 run.py "m1_RCD24_RP24"
 sleep 3
 
 make clean
